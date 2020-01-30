445807755ce53046ff1c34c7f545ab53c7458bb0
shillyshaly <litoxl@hotmail.com>
2020-01-30 14:12:39 -0800

    Excelsior!!!

--- src/Coin.java
+++ src/Coin.java
@@ -1,13 +1,12 @@
+import java.util.*;
+
+
 public class Coin extends Money{
-    public static void main(String[] args) {
-
-    }
-
-    public double getValue() {
-        return 0;
-    }
-
-    public String toString() {
-        return null;
+    private CoinDenomination coinDenomination;
+    public Coin(){
+        super();
+        Random random = new Random();
+        CoinDenomination[] cd = CoinDenomination.values();
+        this.coinDenomination = cd[random.nextInt(cd.length)];
     }
 }
